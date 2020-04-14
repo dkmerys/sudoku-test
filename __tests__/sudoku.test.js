f57836be3087042d714a7143250b48df9314e970
Daniel Merys <dkmerys@gmail.com>
2020-04-14 16:25:49 -0700

    add test with meaningful fail for pushRow function

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -1,4 +1,5 @@
 import { numberChecker } from "./../src/sudoku.js";
+import { Row } from "./../src/sudoku.js";
 
 describe('number-checker', () => {
 
@@ -6,5 +7,13 @@ describe('number-checker', () => {
     var inputType = numberChecker(1);
     expect(inputType).toBeLessThan(10);
     expect(inputType).toBeGreaterThan(0);
+  })
+})
+
+describe('pushRow', () => {
+  test ('should correctly push a user entry to the row array', () => {
+    var row1 = new Row ();
+    row1.pushRow(7);
+    expect(row1.rowArray.includes(7)).toEqual(true);
   })
 })
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,5 +1,14 @@
+export function Row() {
+  this.rowArray = [];
+}
+
+
 export var numberChecker = function(userInput) {
   if (typeof(userInput) === "number" && userInput < 10 && userInput > 0) {
     return userInput
   }
 };
+
+Row.prototype.pushRow = function(userInput) {
+  
+}
