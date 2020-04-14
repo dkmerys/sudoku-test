5a2911a2082806d86b86e243ac6a03eff51182b5
Daniel Merys <dkmerys@gmail.com>
2020-04-14 16:07:53 -0700

    add number-checker function

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,3 +1,5 @@
-export var numberChecker = function() {
-  
+export var numberChecker = function(userInput) {
+  if (typeof(userInput) === "number" && userInput < 10 && userInput > 0) {
+    return userInput
+  }
 };
